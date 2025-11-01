1b9bc3069bc51c4430029b21cb2519b6722b1660
Mike <reinstein.mike@gmail.com>
2025-10-31 21:02:01 -0700

    add various things for nvim

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -32,6 +32,9 @@ return { -- Autoformat
 			lua = { "stylua" },
 			-- Conform can also run multiple formatters sequentially
 			-- python = { "isort", "black" },
+			python = { "ruff" },
+
+			go = { "gofumpt" },
 			--
 			-- You can use 'stop_after_first' to run the first available formatter from the list
 			javascript = { "biome", "prettierd", "prettier", stop_after_first = true },
